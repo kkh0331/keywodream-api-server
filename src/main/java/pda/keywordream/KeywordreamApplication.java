58e766228fe1104cf874a61d4f274b3e9dc48214
kihyun.kwon <kkh3272@naver.com>
2024-08-26 18:03:10 +0900

    init: first commit

---
+++ src/main/java/pda/keywordream/KeywordreamApplication.java
@@ -0,0 +1,13 @@
+package pda.keywordream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pda/keywordream/KeywordreamApplicationTests.java
@@ -0,0 +1,13 @@
+package pda.keywordream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
